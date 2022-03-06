160abe3eefe4538e420012f058707692364e5666
ahryniewicz <ahryniewicz@taoglas.com>
2022-03-06 13:55:01 +0100

    import table

--- make_api.py
+++ make_api.py
@@ -1,38 +1,8 @@
 import os
+import sys
 from api_gen import generate_api, CborTypes
 
 current_path = os.path.dirname(os.path.realpath(__file__))
-
-rpc_table_in = {
-    "uplink_enqueue": [CborTypes.CBOR_TYPE_BYTE_STRING],
-    "uplink_enqueue_confirmed": [CborTypes.CBOR_TYPE_BYTE_STRING, CborTypes.CBOR_TYPE_UNSIGNED_INTEGER],
-    "uplink_flush": [],
-    "uplink_count": [],
-    "downlink_pop": [CborTypes.CBOR_TYPE_UNSIGNED_INTEGER],
-    "downlink_push": [CborTypes.CBOR_TYPE_BYTE_STRING],
-    "datetime_get": [],
-    "datetime_set": [CborTypes.CBOR_TYPE_UNSIGNED_INTEGER, CborTypes.CBOR_TYPE_UNSIGNED_INTEGER],
-    "alarm_set": [CborTypes.CBOR_TYPE_UNSIGNED_INTEGER, CborTypes.CBOR_TYPE_BOOL],
-    "alarm_set_delta": [CborTypes.CBOR_TYPE_UNSIGNED_INTEGER],
-    "alarm_clear": [],
-    "gnss_acquire": [],
-    "gnss_is_active": [],
-    "gnss_get_location": [],
-    "bootloader_radio_version": [],
-    "bootloader_start": [],
-    "bootloader_write": [CborTypes.CBOR_TYPE_BYTE_STRING],
-    "bootloader_finish": [],
-    "bootloader_count": [],
-    "sensor_update_version": [],
-    "sensor_update_read": [CborTypes.CBOR_TYPE_UNSIGNED_INTEGER, CborTypes.CBOR_TYPE_UNSIGNED_INTEGER],
-    "radio_state_get": [],
-    "radio_state_reset": [],
-    "radio_counters_get": [],
-    "radio_counters_reset": [],
-    "eui64_get": [],
-    "identifiers_get": [],
-    "log_get": [CborTypes.CBOR_TYPE_UNSIGNED_INTEGER],
-    "reboot": [],
-    "radio_api_version": []
-}
-generate_api(current_path, rpc_table_in)
+sys.path.append(sys.argv[1])
+module = __import__('rpc_funcs_table')
+generate_api(current_path, module.rpc_table_in)
