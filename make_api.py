9a526bbb44411ce72e91c69937a0b699f65c468f
ahryniewicz <ahryniewicz@taoglas.com>
2022-03-06 23:15:27 +0100

    Generate files to the same dir as passed rpc_funcs_table dir

--- make_api.py
+++ make_api.py
@@ -1,8 +1,6 @@
-import os
 import sys
 from api_gen import generate_api, CborTypes
 
-current_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(sys.argv[1])
 module = __import__('rpc_funcs_table')
-generate_api(current_path, module.rpc_table_in)
+generate_api(sys.argv[1], module.rpc_table_in)
